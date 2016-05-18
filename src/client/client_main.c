bd3bbd670023c3346160789e3b7503ce592fd9db
David <david.tsaturyan.95@gmail.com>
2016-05-18 11:50:33 +0400

    Changed Client side
    - Implemented functionality to get SHA1 of a file

--- src/client/client_main.c
+++ src/client/client_main.c
@@ -8,11 +8,15 @@
 #include <unistd.h>
 #include <errno.h>
 #include <openssl/sha.h>
+#include <fcntl.h>
 
 #include "client.h"
 
 #define DATA_SIZE 100
 #define BUFFER_SIZE 2000
+
+
+int sha1_of_a_file(int, unsigned char*);
 
 int main(int argc, char* argv[])
 {
@@ -36,7 +40,7 @@ int main(int argc, char* argv[])
 
     char server_reply[BUFFER_SIZE];
     memset(server_reply, 0, BUFFER_SIZE);
-    if ( read(sock_fd, server_reply, BUFFER_SIZE) < 0 )
+    if (read(sock_fd, server_reply, BUFFER_SIZE) < 0 )
     {
 	fprintf(stderr, "%s\n", strerror(errno));
 	return 1;
@@ -45,28 +49,17 @@ int main(int argc, char* argv[])
 
     while(1)
     {
-	fgets(data_string, (DATA_SIZE - 1), stdin);
+	//fgets(data_string, (DATA_SIZE - 1), stdin);
 
-	if(errno != 0)
+	unsigned char hash[SHA_DIGEST_LENGTH] = {0};
+
+	int err = sha1_of_a_file(sock_fd, hash);
+
+	if(err == 1)
 	{
-	    fprintf(stderr, "%s\n", strerror(errno));
+	    fprintf(stderr, "%s\n", "err");
 	    return 1;
 	}
-
-	if( write(sock_fd, data_string, strlen(data_string)) == -1 )
-	{
-	    fprintf(stderr, "%s\n", strerror(errno));
-	    return 1;
-	}
-
-	unsigned char hash[SHA_DIGEST_LENGTH];
-	if ( read(sock_fd, hash, SHA_DIGEST_LENGTH) < 0 )
-	{
-	    fprintf(stderr, "%s\n", strerror(errno));
-	    return 1;
-	}
-
-
 
 	for(int i = 0; i<SHA_DIGEST_LENGTH; ++i)
 	{
@@ -79,4 +72,68 @@ int main(int argc, char* argv[])
     close(sock_fd);
     return 0;
 }
+int sha1_of_a_file(int sock_fd, unsigned char* hash)
+{
+    int fd;
+    char path[255] = { 0 };
+    char buf[DATA_SIZE] = {0};
+    
+    puts("Enter the path of a file");
+    
+    fgets(path, 254, stdin);
+    fprintf(stderr, "%s\n", path);
 
+    path[strlen(path) - 1] = '\0';
+    if( (fd = open(path, O_RDONLY)) < 0 )
+    {
+	fprintf(stderr, "%s\n", strerror(errno));
+	return 1;
+    }
+	 
+    while(1)
+    {
+	int num_read;
+	char* p = buf;
+	    
+	num_read = read(fd, buf, DATA_SIZE - 1);
+	 
+	if(num_read < 0)
+	{
+	    fprintf(stderr, "%s\n", strerror(errno));
+	    return 1;
+	}
+		
+	if(num_read == 0)
+	{
+	    break;
+	}
+		    
+	while(num_read > 0)
+	{
+	    int num_write =  write(sock_fd, p, num_read);
+	    if(num_write <= 0)
+	    {
+		fprintf(stderr, "%s\n", strerror(errno));
+		return 1;
+	    }
+	    
+	    num_read -= num_write;
+	    p += num_write;
+	}	
+
+	memset(buf, 0, DATA_SIZE); 
+    }
+    
+   // fprintf(stderr, "%s\n", "writing EOF");
+   // fprintf(stderr, "%s\n", "-----------");
+    write(sock_fd, "EOF", 4);
+   // fprintf(stderr, "%s\n", "-----------");
+	
+    if ( read(sock_fd, hash, SHA_DIGEST_LENGTH) < 0 )
+    {
+	fprintf(stderr, "%s\n", strerror(errno));
+	return 1;
+    }
+    
+    return 0;
+}
