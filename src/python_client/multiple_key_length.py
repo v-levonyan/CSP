99909cf864dec2b2f83fe1bbc947fe15ae2429b2
David <david.tsaturyan.95@gmail.com>
2016-08-18 14:53:13 +0400

    multiple_key_length.py

--- src/python_client/multiple_key_length.py
+++ src/python_client/multiple_key_length.py
@@ -18,65 +18,68 @@ def connection(clientSock, host = '127.0.0.1', port = 8888):
 
     clientSock.connect(host, port)
     rec = clientSock.recieveMessage()
-    print 'rec ', rec, ' rec'
-    clientSock.sendMessage('0')
-
-
+ 
 def registration(clientSock, user_name, password):
    
     clientSock.sendMessage('0')
-    if client.check_username(user_name) != 0:
-	clientSock.shutDownAndClose()
-	return FAILURE 
-   
+  
     clientSock.sendMessage(user_name)
     
     if int(clientSock.recieveMessage()) == 1:
 	return FAILURE
-
-    #return free_or_busy
-
-    if client.check_password(password) != 0:
-	clientSock.shutDownAndClose()
-	return FAILURE
-    
+   
     clientSock.sendMessage(password)
    
     clientSock.recieveMessage()#timeout = 1)
     
     return SUCCESS
 
+
 def demand_services(clientSock):
       
       clientSock.sendMessage("CSP1.0://Get Services")
 
       while 1:
-          services = clientSock.recieveMessage()
           
+	  services = clientSock.recieveMessage()
+  
 	  if services == 'END':
               break
           else:
 	      continue
 
-def test_key_generation():
 
+
+def symmetric_key(clientSock, num):
+ 
+         CorrespondingKey = { '3' : '7', '4' : '21', '5' : '16', '6' : '24', '7' : '32' }
+         size = CorrespondingKey.get(num)
+ 
+         seq = (num, size)
+         params = ':'.join(seq)
+ 
+         clientSock.sendMessage(params)
+         return  clientSock.recieveMessage()
+	
+def get_key_ID(clientSock):
+       	
+    return symmetric_key(clientSock,'7')
+     	
+
+def test_AES_encryption():
+    
     ctx = client.context() 
     clientSock = client.clientSocket(ctx)
     
     connection(clientSock)
-       
-#    assert registration(clientSock, user_name = 'David', password = 'david') == SUCCESS
+   
+    registration(clientSock, 'David', 'david')
     
-    registration(clientSock, user_name = 'David', password = 'david') 
-    
-    for i in range(0,100):
-	
-	clientSock.symmetric_key('7')
-	
-	assert 1
+    demand_services(clientSock)
+   
+    for i in range (0, 100):
+	assert len(get_key_ID(clientSock)) == 64
+   
 	demand_services(clientSock)
-	
-
-#    assert 0
 
 
